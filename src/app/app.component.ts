93fce480bbd91666f4306a7ce620a83ab7c42564
kockono <kockonoop@gmail.com>
2023-01-14 17:42:24 -0600

    funciones para editar y extraer excel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,6 @@ export class AppComponent {
 
     ExcelData:any;
 
-    constructor() {
-
-    }
 
     LeerExcel(archivo:any) {
         let file = archivo?.target.files[0];
@@ -29,5 +26,36 @@ export class AppComponent {
             // XLSX.utils.sheet_to_csv
         }
 
+    }
+
+    editarId(id:any) {
+
+    }
+    editarNombre(nombre:any, index:number) {
+      console.log(`AppComponent ~ editarNombre ~ index`, index)
+      console.log(`AppComponent ~ editarNombre ~ nombre`, nombre)
+
+      this.ExcelData[index].Nombre = this.ExcelData[index].Nombre.toLowerCase();
+
+    }
+    editarRecurso(recurso:string) {
+
+    }
+    edtarFecha(fecha:string) {
+
+    }
+
+    crearArchivo() {
+      // Acquire Data (reference to the HTML table)
+      var table_elt = document.getElementById("my-table-id");
+
+      // Extract Data (create a workbook object from the table)
+      var workbook = XLSX.utils.table_to_book(table_elt);
+
+      // Process Data (add a new row)
+      var ws = workbook.Sheets["Sheet1"];
+      XLSX.utils.sheet_add_aoa(ws, [["Created "+new Date().toISOString()]], {origin:-1})
+
+      XLSX.writeFile(workbook, "Report.xlsx");
     }
 }
